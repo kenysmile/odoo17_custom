cc85594dfb553052bb6d2b5a050571ddc06a6a1b
tupv <dendroidking02121@gmail.com>
2025-04-09 21:50:58 +0700

    main

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Tupv',
+    'version': '1.3',
+    'category': 'Tupv',
+    'sequence': 215,
+    'summary': 'Tupv',
+    'website': 'https://www.odoo.com/app/appointments',
+    'description': """
+Tupv
+    """,
+    'depends': ['web', 'base'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'OEEL-1',
+    'assets': {
+        'web.assets_frontend': [
+        ],
+        'web.assets_backend': [
+        ],
+        'web_editor.backend_assets_wysiwyg': [
+        ],
+        'web.qunit_suite_tests': [
+        ],
+    }
+}
